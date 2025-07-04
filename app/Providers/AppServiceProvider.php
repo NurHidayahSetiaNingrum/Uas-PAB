8b47bac57251bf205d1d3e3b4567188e1b36889b
NurHidayahSetiaNingrum <dayahstym@gmail.com>
2025-07-04 21:46:36 +0700

    impor kelas

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,8 +4,9 @@ namespace App\Providers;
 
 use App\Models\Post;
 use App\Policies\PostPolicy;
-use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
