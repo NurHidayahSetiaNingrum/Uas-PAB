d00c03e52a10e6dfcfd487a3283427761f6478af
NurHidayahSetiaNingrum <dayahstym@gmail.com>
2025-07-06 21:31:50 +0700

    update kesekian kalinya

--- resources/views/home/articles.blade.php
+++ resources/views/home/articles.blade.php
@@ -15,7 +15,7 @@
           <a href="{{ route('post.detail', ['slug' => $post->slug]) }}" class="inline-block text-white bg-indigo-500 hover:bg-indigo-600 px-4 py-2 rounded text-sm">
         Lihat Selengkapnya
       </a>
-            <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+      <!-- <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round"> -->
               <path d="M5 12h14"></path>
               <path d="M12 5l7 7-7 7"></path>
             </svg>
